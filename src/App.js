842b8071b47e46d609a196b3bdb1d64bdb0e017b
Dinesh Pawar <dsp70458@gmail.com>
2025-02-27 21:58:43 +0530

    make mobile friendly card and home

--- src/App.js
+++ src/App.js
@@ -8,20 +8,26 @@ import Blogs from './pages/Blogs';
 import ContactUs from './pages/ContactUs';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Card from './components/Card'
 import './App.css'
 
 function App() {
   return (
     <Router>
       <Header />  {/* Only include Header here */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/tools" element={<Tools />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/contactus" element={<ContactUs />} />
-      </Routes>
+      <div className="app-container">
+        <Card />
+        <div className="content-area">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/tools" element={<Tools />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/contactus" element={<ContactUs />} />
+          </Routes>
+        </div>
+      </div>
       <Footer />  {/* Only include Footer here */}
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import '../styles/Home.css';
-import Card from '../components/Card';
 import { TbArrowRightCircle } from "react-icons/tb";
 
 
 const Home = () => {
   return (
     <div className="home-container">
-      <Card />
+      
       <div className="home-content">
         <h1>FULL STACK</h1>
         <h1>WEB <span>DEVELOPER</span></h1>
-        <p>Passionate MERN stack developer with expertise in both frontend and backend, crafting scalable and efficient solutions. Experienced in building real-world projects, solving complex challenges, and constantly innovating to create impactful digital experiences.</p>
+        <h3>Passionate MERN stack developer with expertise in both frontend and backend, crafting scalable and efficient solutions. Experienced in building real-world projects, solving complex challenges, and constantly innovating to create impactful digital experiences.</h3>
         <div className="home-stats">
           <div className="home-stat-item">
             <h1>+02</h1>
